4cf7edeb3773657bf77ef2df5950a849586c0fa9
Luis Ngo <baoloc1993@gmail.com>
2015-02-04 15:45:45 +0800

    change main activity to fragment main

--- app/src/main/java/com/example/bernard/silver_productivity/Page4.java
+++ app/src/main/java/com/example/bernard/silver_productivity/Page4.java
@@ -1,33 +1,34 @@
 package com.example.bernard.silver_productivity;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link PosterFragment.OnFragmentInteractionListener} interface
+ * {@link Page4.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link PosterFragment#newInstance} factory method to
+ * Use the {@link Page4#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PosterFragment extends Fragment {
+public class Page4 extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private static final int MAX_TEXT_COUNT = 150;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -35,20 +36,27 @@ public class PosterFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    EditText inputText;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     *
-     * @return A new instance of fragment PosterFragment.
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Page4.
      */
     // TODO: Rename and change types and number of parameters
-    public static PosterFragment newInstance() {
-        PosterFragment fragment = new PosterFragment();
+    public static Page4 newInstance(String param1, String param2) {
+        Page4 fragment = new Page4();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
         return fragment;
     }
 
-    public PosterFragment() {
+    public Page4() {
         // Required empty public constructor
     }
 
@@ -65,91 +73,26 @@ public class PosterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_poster, container,false);
+        View rootView =  inflater.inflate(R.layout.fragment_page4, container, false);
+        configButton (rootView);
+        configInputText (rootView);
 
-        createTopLayout (rootView);
-        createPostContentLayout (rootView);
-        createAnswerLayout(rootView);
-        createBottomLayout (rootView);
+
         return rootView;
-
-
     }
 
     /**
-     * Create Answer Part
-     * 1. List of answers
-     * 1.1 Content of answers
-     * 1.2 Time of submit
-     * 1.3 Number of like
-     * 1.4 Avatar
+     * Config Button
+     * 1. Cancel button
+     * 2. Submit button
      * @param rootView
      */
-    private void createAnswerLayout(View rootView) {
-        ListView listAnswers = (ListView)rootView.findViewById(R.id.list_answer);
-
-    }
-
-    /**
-     * Create Content of Poster Part
-     * 1. Content of poster
-     * 2. Number of likes
-     * 3. Location of poster
-     * 3. Time of submit
-     * 4. Number of answers
-     */
-    private void createPostContentLayout(View rootView) {
-
-    }
-
-    /**
-     * Create bottom layout of the fragment
-     * 1. Comment Field
-     * 2. Post Button
-     * @param rootView
-     */
-    private void createBottomLayout(View rootView) {
+    private void configButton(View rootView) {
         /*
-        Comment Field
+        Cancel Button
          */
-        final EditText commentField = (EditText) rootView.findViewById(R.id.comment_poster);
-
-
-        /*
-        Post button
-         */
-        Button postButton = (Button) rootView.findViewById(R.id.submit_poster);
-        postButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String result = commentField.getText().toString();
-                sendResultToDatabase();
-            }
-        });
-    }
-
-    private void sendResultToDatabase() {
-
-        
-    }
-
-    /**
-     * Create top layout of the fragment
-     * 1. Back button
-     * 2. Title of fragment
-     * 3. Response button
-     * @param rootView
-     *
-     *
-     */
-    private void createTopLayout(View rootView) {
-
-        /*
-        Back Button
-        When button clicked , go back to general poster page
-         */
-        ImageView backButton = (ImageView) rootView.findViewById(R.id.back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        Button cancelButton  = (Button) rootView.findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -157,27 +100,55 @@ public class PosterFragment extends Fragment {
         });
 
         /*
-        Title of fragment
+        Submmit Button
          */
-
-        TextView title = (TextView) rootView.findViewById(R.id.location_poster);
-
-        /*
-        Response Button
-        When button click, go to fragment response (page 4)
-         */
-        ImageView responseButton = (ImageView) rootView.findViewById(R.id.response_button);
-        responseButton.setOnClickListener(new View.OnClickListener() {
+        Button submitButton = (Button) rootView.findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
-                Go to Response Fragment
-                 */
-                Page4 responseFragment = new Page4();
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.container, responseFragment).commit();
+                String contentInput = inputText.getText().toString();
             }
         });
+
+
+    }
+
+    /**
+     * Config Input text
+     * 1. Edit Text
+     * 2. Remaining Text Count
+     * @param rootView
+     */
+    private void configInputText (View rootView){
+
+        /*
+        Remain TextCount
+         */
+        final TextView remainTextCount = (TextView) rootView.findViewById(R.id.remain_count_text);
+
+        /*
+        Input text
+         */
+        EditText inputText = (EditText) rootView.findViewById(R.id.content_input);
+        inputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                   remainTextCount.setText(String.valueOf(MAX_TEXT_COUNT-count) + "characters remaining");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
 
 
     }
@@ -220,5 +191,7 @@ public class PosterFragment extends Fragment {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
     }
+
+
 
 }
