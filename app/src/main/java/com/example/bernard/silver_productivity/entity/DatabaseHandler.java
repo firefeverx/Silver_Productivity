f69d2937e2d7813fb3432d3755ee2c4ca1cddab0
firefeverx <bernarddeng@hotmail.com>
2015-03-10 00:32:29 +0800

    adding of dialog

--- app/src/main/java/com/example/bernard/silver_productivity/entity/DatabaseHandler.java
+++ app/src/main/java/com/example/bernard/silver_productivity/entity/DatabaseHandler.java
@@ -1,7 +1,27 @@
 package com.example.bernard.silver_productivity.entity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Bernard on 9/3/2015.
  */
-public class DatabaseHandler {
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "posterManager",
+    TABLE_CONTACTS = "poster",
+    KEY_ID = "id",
+    KEY_NAME = "name",
+    KEY_PHONE = "phone",
+    KEY_EMAIL = "email",
+    KEY_ADDRESS = "address",
+    KEY_IMAGEURL = "imageUri";
+
+    public DatabaseHandler(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
 }
