db760fb5a8fc8b3f1b59cdd2ab1309d203a3d8fc
firefeverx <bernarddeng@hotmail.com>
2015-03-26 23:31:25 +0800

    Revert "update"
    This reverts commit 141cff3efbd657176c994b270b8f730ea1986bbc.

--- app/src/main/java/com/example/bernard/silver_productivity/entity/DatabaseHandler.java
+++ app/src/main/java/com/example/bernard/silver_productivity/entity/DatabaseHandler.java
@@ -3,6 +3,8 @@ package com.example.bernard.silver_productivity.entity;
 import android.content.Context;
 import android.content.SyncRequest;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -15,9 +17,14 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.SyncHttpClient;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
@@ -25,14 +32,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
+
 
 /**
  * Created by Bernard on 9/3/2015.
  */
-public class DatabaseHandler  {
+public class DatabaseHandler extends Observable {
 
 //    private static final int DATABASE_VERSION = 1;
 //
@@ -47,7 +57,8 @@ public class DatabaseHandler  {
 
     public static final String WEBSERVICE = "http://10.0.2.2/Silver";
     private static final String FILENAME = "C:\\Users\\GuoLong\\Desktop\\ver3\\StraightA_app\\data\\User.txt";
-    public static List<Poster> tmpposterList  = new ArrayList<Poster>();
+    public static ArrayList<Poster> tmpposterList = new ArrayList<Poster>();
+
     private Gson gson = new Gson();
 
     public DatabaseHandler() {
@@ -56,24 +67,21 @@ public class DatabaseHandler  {
 
     private void printErrorMessage(int statusCode) {
         String errorMessage = "Error : ";
-        if(statusCode == 404) {
+        if (statusCode == 404) {
             errorMessage += "Requested resource not found";
-        }
-        else if(statusCode == 500) {
+        } else if (statusCode == 500) {
             errorMessage += "Something went wrong at server end";
-        }
-        else {
+        } else {
             errorMessage += "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]";
         }
         //setChanged();
         //notifyObservers(errorMessage);
     }
 
-    public void getForumPostByThread(int threadID) throws IOException
-    {
+    public void getForumPostByThread(int threadID) throws Exception {
         System.out.println("1 getforum");
-        String url = WEBSERVICE +"/get_poster_details.php?id="+threadID;
-        AsyncHttpClient client = new AsyncHttpClient();
+        String url = WEBSERVICE + "/get_poster_details.php?id=" + threadID;
+        //AsyncHttpClient client = new AsyncHttpClient();
 
 //        HttpClient client = new DefaultHttpClient();
 //        System.out.println("before request ");
@@ -100,14 +108,55 @@ public class DatabaseHandler  {
 //            System.out.println("error " + e.getMessage());
 //        }
 
+//        HttpClient client = new DefaultHttpClient();
+//
+//        try
+//        {
+//            String SetServerString = "";
+//
+//            // Create Request to server and get response
+//
+//            System.out.println("2 request");
+//            HttpGet httpget = new HttpGet("http://localhost/Silver/get_poster_details.php?id=1");
+//            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+//            SetServerString = client.execute(httpget, responseHandler);
+//
+//            System.out.println("here " + SetServerString);
+//
+//        }
+//        catch(Exception ex)
+//        {
+//            System.out.println("Fail! " + ex.getMessage());
+//        }
 
+//        List<NameValuePair> params = new ArrayList<NameValuePair>();
+//        // getting JSON string from URL
+//        HttpClient client = new DefaultHttpClient();
+//        HttpGet request = new HttpGet("http://10.0.2.2/Silver/get_poster_details.php?id=1");
+//        HttpResponse httpResponse = client.execute(request);
+//        BufferedReader rd = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+//        String result = "";
+//        while ((result = rd.readLine()) != null){
+//            System.out.println("reading " + result);
+//        }
+//        try {
+//
+//            JSONObject jsonObject = new JSONObject(result);
+//        } catch (Exception e){
+//
+//        }
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
 
         client.get(url, null, new AsyncHttpResponseHandler() {
+
 
             @Override
             public void onFailure(int statusCode, Header[] header, byte[] content, Throwable throwable) {
                 System.out.println("2 " + statusCode);
                 printErrorMessage(statusCode);
+
 
             }
 
@@ -115,6 +164,8 @@ public class DatabaseHandler  {
             public void onSuccess(int statusCode, Header[] header, byte[] content) {
                 System.out.println("3");
                 System.out.println(" success ");
+
+
                 try {
                     //List<ForumPost> planList = new ArrayList<ForumPost>();
                     List<Poster> posterList = new ArrayList<Poster>();
@@ -124,38 +175,114 @@ public class DatabaseHandler  {
                     JsonParser parser = new JsonParser();
                     System.out.println("here " + response);
                     JsonArray data = parser.parse(response).getAsJsonArray();
+                    //Poster poster = null;
+                    //ArrayList<Comment> listcomment = null;
 
+                    System.out.println("testget " + data.size());
 
-                    Iterator<JsonElement> itr = data.iterator();
+                    if(data != null)
+                    {
+                        for (int i = 0; i < data.size(); i++)
+                        {
+                            System.out.println("looping ");
+                            JsonObject jObject = data.get(i).getAsJsonObject();
+                            //System.out.println("output: " + jObject.get("poster").getAsJsonObject().get("title"));
+                           Poster poster = new Poster();
+//
+//                            System.out.print("before ");
+//                        //jObject
+                        poster.setId(jObject.get("poster").getAsJsonObject().get("id").toString());
+//                            System.out.print("after ");
+                        poster.setTitle(jObject.get("poster").getAsJsonObject().get("title").toString());
+//
+                        poster.setContent(jObject.get("poster").getAsJsonObject().get("content").toString());
+//                        //poster.setComments(jObject.get(""));
+//
+                        poster.setAuthor(jObject.get("poster").getAsJsonObject().get("author").toString());
+                        poster.setLocation(jObject.get("poster").getAsJsonObject().get("location").toString());
+                        poster.setTime(jObject.get("poster").getAsJsonObject().get("time").toString());
+//
+                        ArrayList <Comment> listcomment = new ArrayList<Comment>();
+                        Comment comment = new Comment();
+                        comment.setContent(jObject.get("poster").getAsJsonObject().get("comments").toString());
 
-                    while (itr.hasNext()) {
-                        JsonElement jsonElement = itr.next();
-                        JsonObject object = jsonElement.getAsJsonObject();
-                        Poster poster = gson.fromJson(object.get("poster"), Poster.class);
+                        listcomment.add(comment);
+
+                        poster.setComments(listcomment);
+
+//
                         posterList.add(poster);
-                        System.out.println("testget " + itr.next().toString());
+
+                        tmpposterList.add(poster);
+                            System.out.println("before exit loop " + tmpposterList.get(0).getContent());
+
+
+                        }
+                        System.out.println("exit loop " + posterList.get(0).getId());
+                    }
+//                    for (JsonElement element : data){
+//                        System.out.print("entering ");
+//                        poster = new Poster();
+//                        JsonObject jObject = element.getAsJsonObject();
+//                        //System.out.println ("OBJECT " + jObject.get("poster").getAsJsonObject().get("id"));
+//                        //jObject
+//                        poster.setId(jObject.get("poster").getAsJsonObject().get("id").toString());
+//                        poster.setTitle(jObject.get("poster").getAsJsonObject().get("title").toString());
+//
+//                        poster.setContent(jObject.get("poster").getAsJsonObject().get("content").toString());
+//                        //poster.setComments(jObject.get(""));
+//
+//                        poster.setAuthor(jObject.get("poster").getAsJsonObject().get("author").toString());
+//                        poster.setLocation(jObject.get("poster").getAsJsonObject().get("location").toString());
+//                        poster.setTime(jObject.get("poster").getAsJsonObject().get("time").toString());
+//
+//                        listcomment = new ArrayList<Comment>();
+//                        Comment comment = new Comment();
+//                        comment.setContent(jObject.get("poster").getAsJsonObject().get("comments").toString());
+//                        listcomment.add(comment);
+//
+//                        posterList.add(poster);
+//                        tmpposterList.add(poster);
+//
+//                        System.out.print("looping ");
+//                    }
+
+//                    Iterator<JsonElement> itr = data.iterator();
+//                    //System.out.println("inside");
+//                    while (itr.hasNext()) {
+//                        JsonElement jsonElement = itr.next();
+//                        JsonObject object = jsonElement.getAsJsonObject();
+//                        Poster poster = gson.fromJson(object.get("poster"), Poster.class);
+//                        posterList.add(poster);
+//                        System.out.println("testget " + poster.getContent());
+//                    }
+
+                    //tmpposterList = posterList;
+                    System.out.println("size " + tmpposterList.size());
+
+                    setChanged();
+                    notifyObservers(posterList);
+                    }
+                    catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
                     }
 
-                    tmpposterList = posterList;
-
-                    //setChanged();
-                    //notifyObservers(planList);
-
-
-                }
-                catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
 
             }
-        });
+
+        }
+        );
+
+
 //Select * from forumpost where userName=?
 //Select * from forumthread where category=?
 
 //        List<ForumPost> result = new ArrayList<ForumPost>();
 //        ForumDAO forumDAO = new ForumDAO();
 //        result = forumDAO.getPost(threadID);
-            //return tmpposterList;
+        //return tmpposterList;
+
+
     }
 
 
--- app/src/main/java/com/example/bernard/silver_productivity/listPoster.java
+++ app/src/main/java/com/example/bernard/silver_productivity/listPoster.java
@@ -1,17 +1,14 @@
 package com.example.bernard.silver_productivity;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
 import android.widget.AdapterView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,16 +16,20 @@ import com.example.bernard.silver_productivity.entity.DatabaseHandler;
 import com.example.bernard.silver_productivity.entity.Poster;
 
 import java.util.ArrayList;
-import java.io.UnsupportedEncodingException;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class listPoster extends Fragment {
+public class listPoster extends Fragment implements Observer, AdapterView.OnItemClickListener {
 
 
     //public static listPoster activity;
     private static boolean refreshstatus = false;
+    private List<Poster> posterList;
+    private MyAdapter adapter;
+    DatabaseHandler db = new DatabaseHandler();
+    private View rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,7 +37,7 @@ public class listPoster extends Fragment {
         super.onCreate(savedInstanceState);
 
             //activity = this;
-            View rootView = inflater.inflate(R.layout.activity_list_poster, container, false);
+            rootView = inflater.inflate(R.layout.activity_list_poster, container, false);
             // setContentView(R.layout.activity_list_poster);
 
             if(refreshstatus == false) {
@@ -44,24 +45,24 @@ public class listPoster extends Fragment {
 
             //try {
             ArrayList<Poster> posters = new ArrayList<Poster>();
-            Poster poster1 = new Poster("How to cope stress?", "Singapore", 5, 8);
-            Poster poster2 = new Poster("How to communicate with elderly?", "Malaysia", 4, 1);
-            Poster poster3 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            Poster poster4 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            Poster poster5 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            Poster poster6 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            Poster poster7 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            Poster poster8 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            Poster poster9 = new Poster("How to do well in studies?", "Singapore", 3, 5);
-            posters.add(poster1);
-            posters.add(poster2);
-            posters.add(poster3);
-            posters.add(poster4);
-            posters.add(poster5);
-            posters.add(poster6);
-            posters.add(poster7);
-            posters.add(poster8);
-            posters.add(poster9);
+//            Poster poster1 = new Poster("How to cope stress?", "Singapore", 5, 8);
+//            Poster poster2 = new Poster("How to communicate with elderly?", "Malaysia", 4, 1);
+//            Poster poster3 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            Poster poster4 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            Poster poster5 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            Poster poster6 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            Poster poster7 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            Poster poster8 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            Poster poster9 = new Poster("How to do well in studies?", "Singapore", 3, 5);
+//            posters.add(poster1);
+//            posters.add(poster2);
+//            posters.add(poster3);
+//            posters.add(poster4);
+//            posters.add(poster5);
+//            posters.add(poster6);
+//            posters.add(poster7);
+//            posters.add(poster8);
+//            posters.add(poster9);
 
             String[] strPosts = new String[]{"How to cope stress?", "How to communicate with elderly?", "How to do well in studies?"};
             String[] strLocation = new String[]{"Singapore", "Malaysia", "Singapore"};
@@ -74,21 +75,45 @@ public class listPoster extends Fragment {
 //        MyAdapter adapter = new MyAdapter(getActivity(),R.layout.activity_list_poster, posters);
 
 
-            DatabaseHandler db = new DatabaseHandler();
+                DatabaseHandler db = new DatabaseHandler();
+                db.addObserver(this);
+
             //List<Poster> tmp = db.getForumPostByThread(1);
+                //String returned = null;
                 try {
                     db.getForumPostByThread(1);
+
+                    System.out.println("i am here size " + DatabaseHandler.tmpposterList.get(0).getContent());
+
+//                    MyAdapter adapter = new MyAdapter(getActivity(), DatabaseHandler.tmpposterList);
+//
+//                    listPosters.setAdapter(adapter);
+                    //System.out.println("testing " + returned);
                 }catch (Exception e)
                 {
-
+                    System.out.println(e.getMessage());
                 }
-            refreshstatus = true;
+
+
+
+
+
+            //refreshstatus = true;
+
+//                for(int i = 0; i< db.tmpposterList.size(); i++)
+//                {
+//                    Poster post = new Poster(db.tmpposterList.get(i).getContent().toString(), db.tmpposterList.get(i).getLocation(), db.tmpposterList.get(i).getNumberOfComment(), db.tmpposterList.get(i).getNumberOfLike());
+//                    posters.add(post);
+//                    System.out.println(db.tmpposterList.get(i).getContent().toString());
+//                }
 
             //System.out.print("tmp " + tmp.get(0).getAuthor());
-            MyAdapter adapter = new MyAdapter(getActivity(), posters);
+            //MyAdapter adapter = new MyAdapter(getActivity(), posters);
 
 
-            listPosters.setAdapter(adapter);
+//            MyAdapter adapter = new MyAdapter(getActivity(), posters);
+//
+//            listPosters.setAdapter(adapter);
 
 
             listPosters.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -133,8 +158,43 @@ public class listPoster extends Fragment {
         //mListener = null;
     }
 
+    @Override
+    public void update(Observable observable, Object data) {
+        try {
+            if (data instanceof String){
+                String result = (String)data;
+                Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            }
+            else {
+                if (data != null  ) {
+                    posterList = (List<Poster>)data;
 
+                    if (posterList != null) {
+                        //this.forumPostList = listForumPost;
+                        //for(Object o: forumPostList) {
+                        //Toast.makeText(getActivity().getApplicationContext(), ((ForumPost)o).toString(), Toast.LENGTH_LONG).show();
+                        //}
+                        adapter =  new MyAdapter(getActivity(), DatabaseHandler.tmpposterList);
 
+                        ListView listPosters = (ListView) rootView.findViewById(R.id.listItemPosts);
+                        listPosters.setAdapter(adapter);
+                        listPosters.setOnItemClickListener(this);
+//                        ListView listView = (ListView) getView().findViewById(R.id.listForum);
+//                        listView.setAdapter(customAdapter);
+                        //listView.setOnItemClickListener(this);
+                    }
+                }
+            }
+        }
+        catch (Exception ex) {
+            Toast.makeText(getActivity().getApplicationContext(), "Something went wrong while processing request..", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 
 
     /**
@@ -150,6 +210,21 @@ public class listPoster extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    private class loadDataBackground extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                db.getForumPostByThread(1);
+                //System.out.println("testing " + returned);
+            }catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+            return null;
+        }
     }
 }
 
